Fixed Rotation Of Drone

controllers/gesture_controller.py
@@ -1,13 +1,16 @@
 from djitellopy import Tello
 from utils import GestureControl
 import numpy as np
+from tensorflow.keras.models import load_model, Sequential
 
 
-def gesture_controller(frame: np.ndarray, tello: Tello, gesture_control: GestureControl, threshold: float = 0.5,
+def gesture_controller(frame: np.ndarray, tello: Tello, gesture_control: GestureControl, model: Sequential,
+                       threshold: float = 0.5,
                        debug: bool = False):
     """
     Control the drone using gestures.
 
+    :param model: Model used for prediction
     :param threshold: Minimum value / probability for drone to react to gesture
     :param debug: Decide if the predicted gesture should be printed
     :param frame: Image to predict the frame on
@@ -17,29 +20,40 @@ def gesture_controller(frame: np.ndarray, tello: Tello, gesture_control: Gesture
     :param gesture_control: GestureControl object which performs the guessing
     """
 
-    # key value pair for gesture -> command
-    command = {
-        'up': f'rc {0} {0} {50} {0}', 'down': f'rc {0} {0} {-50} {0}',
-        'left': f'rc {-50} {0} {0} {0}', 'right': f'rc {50} {0} {0} {0}'
+    tracker = gesture_control.hand_tracker
+
+    commands = {
+        '4': f'rc {-25} {0} {0} {0}', '2': f'rc {0} {0} {25} {0}',
+        '5': f'rc {25} {0} {0} {0}', '3': f'rc {0} {0} {-25} {0}', '0-4': f'rc {0} {0} {0} {-100}',
+        '0-5': f'rc {0} {0} {0} {100}', '0-2': f'rc {0} {100} {0} {0}', '0-3': f'rc {0} {-100} {0} {0}'
     }
 
-    gesture, probability = gesture_control.predict(frame)
-    print(probability)
-    if gesture and probability >= threshold:
-        try:
-            if debug:
-                print(f'[DEBUG] Gesture ➡ {gesture}', end='\r')
-
-            if gesture == 'stop':  # takeoff / land
-                if tello.is_flying:
-                    tello.land()
-                else:
-                    tello.takeoff()
-            elif gesture in command.keys():
-                tello.send_command_without_return(command[gesture])
-            else:
-                tello.send_command_without_return('rc 0 0 0 0')
-        except Exception as error:
-            print(f'[ERROR]: {error}')
-    elif tello.is_flying:
+    tracker.hands_finder(frame)
+    if tracker.results.multi_hand_landmarks is None:
         tello.send_command_without_return('rc 0 0 0 0')
+        return
+
+    result: list = []
+    for i in range(0, len(gesture_control.hand_tracker.results.multi_hand_landmarks)):
+        lms = gesture_control.hand_tracker.position_finder(frame, hand_no=i, normalized=True)
+
+        if lms:
+            landmark = np.array(lms).reshape((42,))
+            predictions = model.predict(np.array([landmark]))
+            predicted = np.argmax(np.squeeze(predictions))
+            if np.max(np.squeeze(predictions)) < threshold:
+                tello.send_command_without_return('rc 0 0 0 0')
+                return
+            result.append(int(predicted))
+    key = '-'.join(str(e) for e in sorted(result))
+    print(key)
+    if key == '0':
+        if tello.is_flying:
+            tello.send_command_without_return('land')
+        else:
+            tello.send_command_without_return('takeoff')
+        return
+
+    if key in commands.keys():
+        tello.send_command_without_return(commands[key])
+

main.py
@@ -78,6 +78,12 @@ def create_dataset(cap: cv2.VideoCapture, tracker: HandTracker, output: str = 'd
 def predict_multiple_gestures(cap: cv2.VideoCapture, tracker: HandTracker, model_path):
     model: Sequential = load_model(model_path)
 
+    commands = {
+        '0': f'rc {0} {0} {0} {0}', '4': f'rc {0} {0} {0} {0}', '2': f'rc {0} {0} {0} {0}',
+        '5': f'rc {0} {0} {0} {0}', '3': f'rc {0} {0} {0} {0}', '0-4': f'rc {0} {0} {0} {-50}',
+        '0-5': f'rc {0} {0} {0} {50}'
+    }
+
     while True:
         _, frame = cap.read()
 
@@ -85,7 +91,7 @@ def predict_multiple_gestures(cap: cv2.VideoCapture, tracker: HandTracker, model
         if tracker.results.multi_hand_landmarks is None:
             continue
 
-        result = []
+        result: list = []
         for i in range(0, len(tracker.results.multi_hand_landmarks)):
             lms = tracker.position_finder(frame, hand_no=i, normalized=True)
 
@@ -93,8 +99,12 @@ def predict_multiple_gestures(cap: cv2.VideoCapture, tracker: HandTracker, model
                 landmark = np.array(lms).reshape((42,))
                 predictions = model.predict(np.array([landmark]))
                 predicted = np.argmax(np.squeeze(predictions))
-                result.append(predicted)
-        print(result)
+                result.append(int(predicted))
+        key = '-'.join(str(e) for e in sorted(result))
+
+        if key in commands.keys():
+            print(result)
+            print(commands[key])
         cv2.imshow('main', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break

utils.py
@@ -103,10 +103,11 @@ class GestureControl:
         self.classes = classes
         self.hand_tracker = hand_tracker
 
-    def predict(self, frame: np.ndarray, debug: bool = False) -> Tuple[Union[str, None], Union[int, None]]:
+    def predict(self, frame: np.ndarray, debug: bool = False, return_predictions: bool = False):
         """
         Predict if what class is in a frame
 
+        :param return_predictions: Whetever to return index or class prediction
         :param debug: Print time used to predict a frame
         :type debug: bool
         :param frame: Ndarray to operate on
@@ -121,6 +122,8 @@ class GestureControl:
         predictions = self.model.predict(np.array([landmark]))
 
         class_id = np.argmax(np.squeeze(predictions))
+        if return_predictions:
+            return predictions
         return self.classes[class_id], np.max(np.squeeze(predictions))
 
 